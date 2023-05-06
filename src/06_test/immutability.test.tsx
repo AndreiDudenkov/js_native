ebccc2865df3ddbddaac36a41d8e8e837fe2937b
AndreiDudenkov <andrei634d@gmail.com>
2023-05-06 21:57:30 +0300

    immutability

--- src/06_test/immutability.test.tsx
+++ src/06_test/immutability.test.tsx
@@ -1,26 +1,128 @@
-import {UserType} from './immutability';
+import {
+    addCompany,
+    CompaniesType,
+    makeHairStyle,
+    removeCssBook,
+    updatedBooks,
+    updateLaptop,
+    UserType,
+    UserWithBooksType,
+    UserWithLaptop
+} from './immutability';
 import {Simulate} from 'react-dom/test-utils';
 import copy = Simulate.copy;
 
 
-const hairdresser = (u:UserType, power:number) =>{
-const copy = {
-    ...u
-}
-     copy.hair = u.hair/power
-    return copy
-}
-
-test ('reference type test', () => {
+test('reference type test', () => {
     let user: UserType = {
         name: 'Andrei',
         hair: 32,
         address: {
-            title: 'Vitebsk'
+            title: 'Vitebsk',
+            house: 12
+
         }
     }
 
-    const updatedUser = hairdresser(user, 2)
+    const updatedUser = makeHairStyle(user, 2)
     expect(updatedUser.hair).toBe(16)
+    expect(user.hair).toBe(32)
 })
+
+test('change address', () => {
+    let user: UserWithLaptop = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            title: 'Vitebsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    }
+    // const updatedUser = moveUser(user, 'Kiev')
+    //
+    // expect(updatedUser).not.toBe(user)
+    // expect(updatedUser.laptop.title).toBe(user.laptop.title)
+    // expect(user.address.title).not.toBe(updatedUser.address.title)
+
+})
+test('update laptop to MacBook', () => {
+    let user: UserWithLaptop = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            title: 'Vitebsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    }
+    const updatedUser = updateLaptop(user, 'MacBook')
+    expect(updatedUser).not.toBe(user)
+    expect(updatedUser.laptop.title).not.toBe(user.laptop.title)
+    expect(updatedUser.laptop.title).toBe('MacBook')
+
+
+})
+test('updatedBooks', () => {
+    let user: UserWithLaptop & UserWithBooksType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            title: 'Vitebsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: ['html', 'css', 'js']
+    }
+    const updatedUserBooks = updatedBooks(user, 'ts')
+
+    expect(updatedUserBooks.books[2]).toEqual('ts')
+    expect(user.books[2]).toEqual('js')
+
+})
+test('removeCssBook', () => {
+    let user: UserWithLaptop & UserWithBooksType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            title: 'Vitebsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: ['html', 'css', 'js']
+    }
+    const updatedUserBooks = removeCssBook(user, 'css')
+
+    expect(updatedUserBooks.books[1]).toBe('js')
+    expect(user.books[2]).toBe('js')
+
+})
+test('updatedUserCompany', () => {
+    let user: UserWithLaptop & CompaniesType = {
+        name: 'Andrei',
+        hair: 32,
+        address: {
+            title: 'Vitebsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        companies: [{id:1, title: "Epam"}, {id:2, title: "IT-Inc"}]
+    }
+    const updatedUserCompany= addCompany(user, 1, 'Google')
+
+    expect(updatedUserCompany.companies[0]).toEqual({id:1, title: "Google"})
+    expect(user.companies[0]).toEqual({id:1, title: "Epam"})
+
+})
+
 
--- src/06_test/immutability.ts
+++ src/06_test/immutability.ts
@@ -1,5 +1,50 @@
+import {useEffect} from 'react';
+
 export type UserType = {
     name: string
     hair: number
-    address: {title: string}
+    address: {
+        title: string
+        house: number
+    }
 }
+export type LaptopType = {
+    title: string
+}
+
+export  type  UserWithLaptop = UserType & {
+    laptop: LaptopType
+}
+
+export type UserWithBooksType = UserType & {
+    books: Array<string>
+}
+export type CompaniesType = {
+    companies: {id:number, title: string}[]
+}
+export const makeHairStyle = (u: UserType, power: number) => {
+    const copy = {
+        ...u,
+        hair: u.hair / power
+    }
+    return copy
+}
+export const updateLaptop = (u: UserWithLaptop, newLaptop: string) => {
+    return {
+        ...u, laptop: {...u.laptop, title: newLaptop}
+    }
+
+}
+
+export const updatedBooks = (u: UserWithLaptop & UserWithBooksType, book: string) => (
+    {...u, books: u.books.map(el => el === 'js' ? el = book : el)}
+)
+export const removeCssBook = (u: UserWithLaptop & UserWithBooksType, book: string) => (
+    {...u, books: u.books.filter
+        (el => el !== book)}
+)
+export const addCompany = (u: UserWithLaptop & CompaniesType, id:number, newtitle: string ) => (
+    {...u, companies: [...u.companies.map(el=>el.id === id
+            ? {...el, title:newtitle}
+            : el)]}
+)
