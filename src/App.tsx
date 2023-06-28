c8f2b1d1f59a7bca5eb86714f6fc12a83cfd0af9
AndreiDudenkov <andrei634d@gmail.com>
2023-06-28 23:50:44 +0300

    objects constructor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Promise} from './promise/Promise';
 
 function App() {
 
   return (
     <div className="App">
+      <Promise/>
     </div>
   )
 }
